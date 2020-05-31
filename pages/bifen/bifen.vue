68184cea95e3524ceae022346df1a645f1ab0c38
hangaoke1 <188869009@qq.com>
2020-05-31 10:39:58 +0800

    feat: 安卓适配

--- pages/bifen/bifen.vue
+++ pages/bifen/bifen.vue
@@ -1,12 +1,12 @@
 <template>
-  <view></view>
+  <view style="height: 100vh;"></view>
 </template>
 <script>
-  
 var wv;
-
+import { openUrl } from '@/util/index.js';
 export default {
-  onLoad() {
+  mounted() {
+    console.log('>>> 加载比分webview');
     const info = uni.getSystemInfoSync();
     const gifRatio = 658 / 494;
     const width = info.screenWidth;
@@ -15,20 +15,31 @@ export default {
 
     //获取当前页面的webview对象
     const currentWebview = this.$mp.page.$getAppWebview();
- 
-    //创建比分webview
-    const url = this.getUrl()
-    wv = plus.webview.create(url, 'custom-webview', {
-      'uni-app': 'none',
-      plusrequire: 'ahead',
-      top: uni.getSystemInfoSync().statusBarHeight
-    }, {
-      url: url // 用于错误页面重新加载使用
-    });
 
-    
+    //创建比分webview
+    const url = this.getUrl();
+    wv = plus.webview.create(
+      url,
+      'custom-webview',
+      {
+        'uni-app': 'none',
+        plusrequire: 'ahead',
+        top: uni.getSystemInfoSync().statusBarHeight
+      },
+      {
+        url: url // 用于错误页面重新加载使用
+      }
+    );
+
     currentWebview.append(wv);
-    
+
+    if (uni.getSystemInfoSync().platform == 'android') {
+      // setTimeout(() => {
+      //   wv.setStyle({ top: uni.getSystemInfoSync().statusBarHeight, bottom: 20 });
+      // }, 1000);
+       wv.setStyle({ top: uni.getSystemInfoSync().statusBarHeight });
+    }
+
     wv.addEventListener(
       'loaded',
       function() {
@@ -38,31 +49,32 @@ export default {
         }
       },
       false
-    ); 
+    );
   },
   methods: {
-    getUrl () {
-      return '/static/redirect.html?url=' + encodeURIComponent('https://live.m.500.com/home/zq/jczq/cur?render=local')
+    getUrl() {
+      return (
+        '/static/redirect.html?url=' +
+        encodeURIComponent('https://live.m.500.com/home/zq/jczq/cur?render=local')
+      );
     },
-    reload () {
-      wv.reload(true)
+    reload() {
+      wv.reload(true);
     },
-    showMore () {
+    showMore() {
       uni.showActionSheet({
-      	itemList: ['刷新'],
-      	success: (res) => {
-          if(res.tapIndex == 0){
-          	this.reload()
+        itemList: ['刷新'],
+        success: res => {
+          if (res.tapIndex == 0) {
+            this.reload();
           } else {
-            plus.runtime.openURL(wv.getURL())
+            plus.runtime.openURL(wv.getURL());
           }
         }
-      })
+      });
     }
   }
 };
 </script>
 
-<style lang="less">
-
-</style>
+<style lang="less"></style>
