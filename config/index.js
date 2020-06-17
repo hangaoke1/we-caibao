5523fdd1a980279d0b82266ea377caa59bb87e45
hangaoke1 <188869009@qq.com>
2020-06-17 10:48:40 +0800

    Feat: update

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 export default {
-  name: '',
-  nameZh: '',
-  server: '',
-  appid: 'wid04087891'
+  name: '',            // app中文名
+  nameZh: '',          // app英文名
+  server: '',          // 服务器地址
+  appid: 'wid04087891' // appid请勿修改
 }
